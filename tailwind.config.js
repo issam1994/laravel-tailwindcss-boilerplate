099b1c7ff6977f37c4c0a4b019c75d30996a11f6
unknown <issamaitouahmane@gmail.com>
2022-06-23 20:36:21 +0100

    feat(daisyui) : updated tailwindcss config to add some configuration for daisyui plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,4 +36,15 @@ module.exports = {
     plugins: [
         require("daisyui")
     ],
+     // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "light",
+  },
 };
